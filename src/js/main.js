6234938220a1d2fc76aba087ace587b216e30528
Dmitrii Kudriavtsev <36885141+armandupe@users.noreply.github.com>
2019-04-25 13:21:34 +0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
+  // Слайдер
   $(".owl-carousel").owlCarousel({
-    items: 1,
-    autoplay: true,
-    autoplayHoverPause: true,
-    loop: true
+    items: 1
   });
 
   var owl = $(".owl-carousel");
   owl.owlCarousel();
   $(".next-btn").click(function() {
-    owl.trigger("next.owl.carousel");
+    owl.trigger("next.owl.carousel", [600]);
   });
   $(".prev-btn").click(function() {
-    owl.trigger("prev.owl.carousel", [300]);
+    owl.trigger("prev.owl.carousel", [600]);
   });
 
+  // Аккордион
   $(function() {
+    var icons = {
+      header: "arrow",
+      activeHeader: "arrow-active"
+    };
     $("#accordion").accordion({
       collapsible: true,
-      heightStyle: "content"
+      heightStyle: "content",
+      icons: icons
     });
   });
+
+
 });
